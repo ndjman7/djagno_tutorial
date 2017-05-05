eb1f111b5d3ac29b5a1f4d5decfe3dd7e4fadd13
ndjman7 <ndjman7@gmail.com>
2017-05-06 01:03:06 +0900

    Add vote, result fbv

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Choice
 
 
@@ -12,7 +12,18 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    pass
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question.id)
